e63be6731456be123430adc389fb69d450b18c42
Visu4lny <12573950+Visu4lny@users.noreply.github.com>
2025-12-26 12:49:56 +0100

    feat: add RegisterPage component with styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { RegisterPage } from './components/RegisterPage'
 
 function App() {
 
   return (
     <>
       <h1>Plant Tracker</h1>
+      <RegisterPage />
     </>
   )
 }
---
+++ src/components/RegisterPage.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import type { RegisterCredentials } from "../types";
+import { useForm } from "react-hook-form";
+import "./RegisterPage.css";
+
+export function RegisterPage() {
+
+  const {
+      register,
+      handleSubmit,
+      watch,
+      formState: { errors }
+    } = useForm({
+      mode: 'onBlur',
+      defaultValues: {
+        email: '',
+        password: '',
+        confirmedPassword: ''
+      }
+    });
+
+  const password = watch('password');
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const onSubmit = async (data: RegisterCredentials) => {
+    setLoading(true);
+    setError(null)
+    console.log(data);
+    // await api
+    setLoading(false)
+  };
+  
+  return (
+    <div className="RegisterPage">
+      <h2>Register Form</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <span className="input-name">Email:</span>
+        <input
+          type="email"
+          placeholder="email@example.com"
+          {...register('email', { 
+          required: 'Email is required',
+          pattern: {value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: 'Invalid format'}
+          })} />
+        {errors.email && <span className="error">{errors.email.message}</span>}
+
+        <span className="input-name">Password:</span>
+        <input 
+          type="password"
+          {...register('password', { 
+          required: 'Password is required',
+          minLength: { value: 8, message: 'Password needs to be at least 8 characters long'},
+          validate: {
+            hasDigit: (value) => /\d/.test(value) || 'Needs a digit',
+            hasUpperCase: (value) => /[A-Z]/.test(value) || 'Needs uppercase letter',
+            hasSpecial: (value) => /[!@#$%^&*()\-_=+]/.test(value) || 'Needs special character'
+          }
+          })} />
+        {errors.password && <span className="error">{errors.password.message}</span>}
+
+        <span className="input-name">Confirm password:</span>
+        <input
+          type="password"
+          {...register('confirmedPassword', { 
+            required: 'Confirmation password is required',
+            validate: (value) => value === password || 'Passwords must be the same'
+          })} />
+        {errors.confirmedPassword && <span className="error">{errors.confirmedPassword.message}</span>}
+
+        <button type="submit">
+          {loading ? 'Registering...' : 'Register'}
+        </button>
+        {error && <div style={{ color: 'red' }}>{error}</div>}
+      </form>
+    </div>
+  )
+}
