0ce3db269b58a11c1e79e976a055aa9ddb281ae3
Visu4lny <12573950+Visu4lny@users.noreply.github.com>
2025-12-26 13:27:20 +0100

    feat: add LoginPage component with styling

---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,58 @@
+import { useForm } from "react-hook-form";
+import "./LoginPage.css";
+import { useState } from "react";
+import type { LoginCredentials } from "../types";
+
+export function LoginPage() {
+  const {
+      register,
+      handleSubmit,
+      formState: { errors }
+    } = useForm({
+      mode: 'onSubmit',
+      defaultValues: {
+        email: '',
+        password: ''
+      }
+    });
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const onSubmit = async (data: LoginCredentials) => {
+    setLoading(true);
+    setError(null)
+    console.log(data);
+    // await api
+    setLoading(false)
+  };
+
+  return (
+    <div className="LoginPage">
+      <h2>Login Form</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <span className="input-name">Email:</span>
+        <input
+          type="email"
+          placeholder="email@example.com"
+          {...register('email', { 
+          required: 'Email is required'
+          })} />
+        {errors.email && <span className="error">{errors.email.message}</span>}
+
+        <span className="input-name">Password:</span>
+        <input 
+          type="password"
+          {...register('password', {
+            required: 'Password is required'
+          })} />
+        {errors.password && <span className="error">{errors.password.message}</span>}
+
+        <button type="submit">
+          {loading ? 'Logging in' : 'Log in'}
+        </button>
+        {error && <div style={{ color: 'red' }}>{error}</div>}
+      </form>
+    </div>
+  )
+}
